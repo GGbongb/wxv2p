d7a5dd339db0163dadb91fe9a862c12b17a3d6c4
GGbongb <986210216@qq.com>
2024-11-22 09:13:25 +0800

    对URL保密

--- components/activation_manager.py
+++ components/activation_manager.py
@@ -132,8 +132,8 @@ class ActivationManager:
                 "%Y-%m-%d %H:%M:%S"
             )
             #print(expiry_date)
-            additional_time_to_subtract = timedelta(days=6, hours=23, minutes=59)
-            remaining = expiry_date - datetime.now() - additional_time_to_subtract
+            #additional_time_to_subtract = timedelta(days=6, hours=23, minutes=59)
+            remaining = expiry_date - datetime.now() 
             
             # 计算总秒数
             total_seconds = int(remaining.total_seconds())
